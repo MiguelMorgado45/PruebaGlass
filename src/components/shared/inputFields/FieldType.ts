e5b2d535c9ff796ea92a751cf11663964dcdea9d
Eduardo <eduardo.acosta@timeweb.mx>
2022-11-16 17:37:51 -0600

    Modifica FielType.ts

--- src/components/shared/inputFields/FieldType.ts
+++ src/components/shared/inputFields/FieldType.ts
@@ -4,10 +4,6 @@ export type FieldType = {
     name: string,
     label: string,
     placeholder: string,
-<<<<<<< HEAD
     options? : [string, string, string, string?]
-=======
-    options? : [string,string, string],
->>>>>>> operadores-89
     formikState: RecoilState<{}>
 }
